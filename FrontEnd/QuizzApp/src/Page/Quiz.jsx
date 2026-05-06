79cf2833d40582f8820e0f34533c0e82af590161
Eros628 <eroslucagbo314@gmail.com>
2026-05-06 23:53:54 +0800

    refactor: seperate file for quizcard component

--- FrontEnd/QuizzApp/src/Page/Quiz.jsx
+++ FrontEnd/QuizzApp/src/Page/Quiz.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router";
 import '../App.css';
 import Header from "../components/Header";
+import QuizCard from "../components/QuizCard";
 import { useEffect, useState } from "react";
 import { ArrowRight } from "lucide-react";
 
@@ -36,71 +37,3 @@ export function QuizStart({quizItems, currentIndex, timer, setCurrentIndex, user
         </>
     )
 }
-
-
-function QuizCard({quizItems,item, timer, setCurrentIndex, currentIndex, user, setUser}){
-    useEffect(()=>{
-    
-        if(currentIndex > user.answers.length){
-            setUser(prev => {
-                return{
-                    ...prev,
-                    answers: [...prev.answers, null]
-                }
-            });
-        }
-
-        else{
-            return;
-        }
-    },[currentIndex])
-    
-    const setChoice = (userChoice)=>{
-       setUser(prev => {
-         const newAnswers = [...prev.answers];
-         console.log(newAnswers.length);
-         if(newAnswers.length > currentIndex){
-            newAnswers[currentIndex] = userChoice;
-         }
-
-         else{
-            if(currentIndex < quizItems.length){
-                newAnswers.push(userChoice);
-            }
-            
-         }
-
-         return{
-            ...prev,
-            answers: newAnswers
-         }
-       });
-
-    }
-    return(
-        <div className="quiz-card">
-            <div className="question">
-                {item.question}
-            </div>
-            <div className="choices">
-                {item.choices.map((choice, index)=>{
-                    return(
-                    <button onClick={
-                        ()=>{
-                            setChoice(choice);
-                        }
-                    } className="choice" key={index}>
-                        {choice}
-                    </button>
-                    )
-                })}
-            </div>
-             {(timer == 0 || timer == null) &&  <button onClick={()=>{
-                if(currentIndex < quizItems.length - 1){
-                    setCurrentIndex(currentIndex + 1);
-                }
-             }} className="btn-next">Next <ArrowRight /></button>}
-        </div>
-    )
-}
-
